48adde33df431f71eda21de552ba9037eac16507
Tony Lin <tonylin514@gmail.com>
2018-11-08 10:12:32 -0800

    Updated histogram methods

--- data_visualizer.py
+++ data_visualizer.py
@@ -30,18 +30,19 @@ def generate_scatterplot(x_axis, y_axis, xlabel, ylabel):
 
 # Generates a scatterplot
 # x_axis - the X axis of the chart
-# y_axis - the Y axis of the chart
 # xlabel - the label of the x axis
-# ylabel - the label of the y axis
-def generate_histogram(x_axis, y_axis, xlabel, ylabel):
-    title = "Scatterplot for: " + xlabel + " vs. " + ylabel
-    plt.hist(x_axis, y_axis)
+def generate_histogram(x_axis, xlabel):
+    title = "Histogram for: " + xlabel
+    plt.hist(x_axis)
     plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
     plt.title(title)
     plt.show()
 
-# Save scatterplot as a png image
+# Save scatterplot as a png image, to a folder called 'scatterplot'
+# x_axis - the X axis of the chart
+# y_axis - the Y axis of the chart
+# xlabel - the label of the x axis
+# ylabel - the label of the y axis
 def image_scatterplot(x_axis, y_axis, xlabel, ylabel):
     title = "Bar chart for: " + xlabel + " vs. " + ylabel
     plt.scatter(x_axis, y_axis)
@@ -51,3 +52,13 @@ def image_scatterplot(x_axis, y_axis, xlabel, ylabel):
     plt.savefig(fname="scatterplot" + '\\' + xlabel + '_vs_' + ylabel + '.png', format='png')
     plt.close('all')
 
+# Save histogram as a png image, to a folder called 'histogram'
+# x_axis - the X axis of the chart
+# xlabel - the label of the x axis
+def image_histogram(x_axis, xlabel):
+    title = "Histogram for: " + xlabel
+    plt.hist(x_axis)
+    plt.xlabel(xlabel)
+    plt.title(title)
+    plt.savefig(fname="histogram" + '\\' + xlabel + '.png', format='png')
+    plt.close('all')
